0cabed9ba60a29c1b5970d1d1138a92bdf284aca
fromant <fromant65@gmail.com>
2022-09-26 10:50:07 -0300

    Hecha estructura básica y conexión a DB

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConn');
+const PORT = process.env.PORT || 3500;
+
+//connect to Mongo DB
+connectDB();
+
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
